9a7fd4252c9970e31f02c3e39881f8cc64c5f07b
Sarfraz Alam <sarfrazk7858@gmail.com>
2024-09-22 19:38:21 +0530

    header

---
+++ src/components/Header.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const Header = () => {
+  return (
+    <div className="w-full h-24 flex items-center justify-around">
+      <p className="lg:text-3xl text-2xl font-semibold text-green-500 shadow-2xl rounded-full">Ummah<span className="text-slate-700">Unity.com</span></p>
+      <p>Organisation</p>
+    </div>
+  );
+};
+
+export default Header;
